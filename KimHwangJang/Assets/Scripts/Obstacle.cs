cbce6047bad3d4fac51892c0a383db4ef300a80e
sigoto4510 <114120869+sigoto4510@users.noreply.github.com>
2022-11-12 08:04:01 +0900

    Drop by Obstacles
    장애물 파괴시 아이템 드랍

--- KimHwangJang/Assets/Scripts/Obstacle.cs
+++ KimHwangJang/Assets/Scripts/Obstacle.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [SerializeField]
+    GameObject food1, food2, food3;
+
+    [SerializeField]
+    GameObject jewel1, jewel2, jewel3;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +23,35 @@ public class Obstacle : MonoBehaviour
     }
 
     private void OnDestroy() {
-        int rand = Random.Range(0,100);
-        if(rand > 50){
-            Debug.Log("아이템 드랍");
+        int foodRand = Random.Range(0,100);
+        int jewelRand = Random.Range(0,100);
+        
+        if(foodRand < 50){
+            Instantiate(food1,this.transform.position + new Vector3(Random.Range(-3.0f,3.0f), 0, Random.Range(-3.0f,3.0f)),Quaternion.identity);
+            Debug.Log("음식 1드랍");
+        }
+        else if(foodRand >= 51 && foodRand < 80) {
+            Instantiate(food2,this.transform.position + new Vector3(Random.Range(-3.0f,3.0f), 0, Random.Range(-3.0f,3.0f)),Quaternion.identity);
+            Debug.Log("음식 2드랍");
         }
         else{
-            Debug.Log("아이템 미드랍");
+            Instantiate(food3,this.transform.position + new Vector3(Random.Range(-3.0f,3.0f), 0, Random.Range(-3.0f,3.0f)),Quaternion.identity);
+            Debug.Log("음식 3드랍");
         }
+
+        if(jewelRand < 50){
+            Instantiate(jewel1,this.transform.position + new Vector3(Random.Range(-3.0f,3.0f), 0, Random.Range(-3.0f,3.0f)),Quaternion.identity);
+            Debug.Log("보석 1드랍");
+        }
+        else if(jewelRand >= 51 && jewelRand < 80) {
+            Instantiate(jewel2,this.transform.position + new Vector3(Random.Range(-3.0f,3.0f), 0, Random.Range(-3.0f,3.0f)),Quaternion.identity);
+            Debug.Log("보석 2드랍");
+        }
+        else{
+            Instantiate(jewel3,this.transform.position + new Vector3(Random.Range(-3.0f,3.0f), 0, Random.Range(-3.0f,3.0f)),Quaternion.identity);
+            Debug.Log("보석 3드랍");
+        }
+
     }
 }
 
